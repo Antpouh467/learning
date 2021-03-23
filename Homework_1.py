ec63cd7346e0d91d42a1baae189129eecc6eacf7
Antpouch467 <antpouch467@gmail.com>
2021-03-23 19:48:17 +0500

    First homeworks from 3 different lessons (all within one day)

---
+++ Homework_1.py
@@ -0,0 +1,7 @@
+FirstAlgebra = (18 + 1 / 10 ) * (30 + 33 / 44 + 50 + 55 / 88) / (9 / 10)
+print(FirstAlgebra)
+SecondAlgebra = sum(range(88888888))
+print(SecondAlgebra)
+ryad = [3, 4, 56, 100, 2, 2, 3]
+ThirdAlgebra = sum(ryad) / len(ryad)
+print(ThirdAlgebra)
