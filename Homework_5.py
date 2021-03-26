e6de908fe8f7a18ceada82f8aca382ef940dd6a6
Antpouch467 <antpouch467@gmail.com>
2021-03-26 18:07:51 +0500

    Задание №5 из видео "Python для начинающих 4" Подзадание №1: Используя numpy, посчитать сумму ряда 0-100 Подзадание №2: Используя numpy, посчитать сумму ряда 0-input(100) Подзадание №3: Используя numpy, посчитать среднее среди 100 случайных чисел

---
+++ Homework_5.py
@@ -0,0 +1,14 @@
+# Задание из видео "Python для начинающих 4"
+# Подзадание №1: Используя numpy, посчитать сумму ряда 0-100
+import numpy
+determined_array = numpy.array(range(100))
+print(numpy.sum(determined_array))
+# Подзадание №2: Используя numpy, посчитать сумму ряда 0-input(100)
+import numpy
+undetermined_array = numpy.array(range(int(input())))
+print(numpy.sum(undetermined_array))
+# Подзадание №3: Используя numpy, посчитать среднее среди 100 случайных чисел
+random_array = numpy.random.rand(1,100)
+# т.к. не было указано, какой тип чисел, случайные числа от 0 до 1 тоже должны засчитаться
+print("Сгенерированные числа: ", random_array)
+print("Сумма: ", numpy.sum(random_array))
