c58bf7f269448da6219e80847d342d44127efabb
Antpouch467 <antpouch467@gmail.com>
2021-04-09 19:09:30 +0500

    Коммит для задания из видео "Python для начинающих 8". Ограничение на длину строки кода определенно помогает при читаемости.

--- Homework_9.py
+++ Homework_9.py
@@ -1,8 +1,8 @@
 # Подзадание №1: Заменить все буквы "x" на "y"
 input_line = input("Введите строку для замены:")
-output_line = "";
+output_line = ""
 # создали пустую строку для замены
-for i in input_line: # проходим по элементам строки
+for i in input_line:  # проходим по элементам строки
     if i == "x":
         output_line += "y"
     else:
@@ -11,18 +11,20 @@ print("Строка после замены: ", output_line)
 print("-----------")
 
 # Подзадание №2: Сосчитать произведение чисел, кратных 3 и 5
-init_list_of_numbers = []
-result_mult = 1
-for i in range(int(input("Количество чисел, из которых надо будет перемножить кратные 3 и 5: "))):
-    init_list_of_numbers.append(int(input()))
-for i in init_list_of_numbers:
+input_numbers = []
+result = 1
+print("Количество чисел, из которых надо будет перемножить кратные 3 и 5: ")
+for i in range(int(input())):
+    input_numbers.append(int(input()))
+for i in input_numbers:
     if i // 3 == i / 3 and i // 5 == i / 5:
         print("Число, кратное 3 и 5: ", i)
-        result_mult *= i
-print("Получившееся произведение: ", result_mult)
+        result *= i
+print("Получившееся произведение: ", result)
 print("-----------")
 
-# Подзадание №3: Заменить буквы в исходной строке без использования дополнительной
+# Подзадание №3: Заменить буквы в исходной строке без использования
+# дополнительной строки
 print("Замена x на y в строке другим способом:")
 print("Исходная строка: ", input_line)
 input_line = input_line.replace("x", "y")
